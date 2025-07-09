3096c8e9a17f825471612ade8fec72480e827435
lekalache <kalachea@live.fr>
2025-07-10 00:24:36 +0200

    feat(theme): translate proxio config to French
    To align the theme's user-facing strings with the site's primary language (fr-FR), all hardcoded values in the `proxio` theme's configuration file have been translated from Chinese and English to French.

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -3,17 +3,17 @@
  */
 const CONFIG = {
     PROXIO_WELCOME_COVER_ENABLE: true, //是否显示页面进入的欢迎文字
-    PROXIO_WELCOME_TEXT: '欢迎来到此网站，点击任意位置进入', // 欢迎文字，留空则不启用
+    PROXIO_WELCOME_TEXT: 'Bienvenue sur ce site, cliquez n\'importe où pour entrer', // 欢迎文字，留空则不启用
 
     // 英雄区块导航
     PROXIO_HERO_ENABLE: true, // 开启英雄区
-    PROXIO_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
-    PROXIO_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
+    PROXIO_HERO_TITLE_1: 'Outil de création de site web gratuit et open source basé sur Notion', // 英雄区文字
+    PROXIO_HERO_TITLE_2: 'Créez votre site web facilement avec vos notes et multipliez votre valeur', // 英雄区文字
     // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-    PROXIO_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
+    PROXIO_HERO_BUTTON_1_TEXT: 'Commencer l\'expérience', // 英雄区按钮
     PROXIO_HERO_BUTTON_1_URL:
         'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
-    PROXIO_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
+    PROXIO_HERO_BUTTON_2_TEXT: 'Suivez-nous sur Github', // 英雄区按钮
     PROXIO_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
     PROXIO_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
 
@@ -24,9 +24,9 @@ const CONFIG = {
 
     // 文章区块
     PROXIO_BLOG_ENABLE: true, // 首页博文区块开关
-    PROXIO_BLOG_TITLE: '作品',
+    PROXIO_BLOG_TITLE: 'Réalisations',
     PROXIO_BLOG_COUNT: 4, // 首页博文区块展示前4篇文章
-    PROXIO_BLOG_TEXT_1: '我的最新动态',
+    PROXIO_BLOG_TEXT_1: 'Mes dernières actualités',
 
     // 区块默认内容显示文章的summary文本，但也支持用自定义图片或logo德国替换掉占位显示内容
     PROXIO_BLOG_PLACEHOLDER_IMG_URL_1: '', // 填写要替换成的图片，支持图床或直接上传到项目中，示例  /images/feature-1.webp
@@ -39,161 +39,161 @@ const CONFIG = {
 
     // 特性区块
     PROXIO_FEATURE_ENABLE: true, // 特性区块开关
-    PROXIO_FEATURE_TITLE: '为什么选我',
-    PROXIO_FEATURE_TEXT_1: '我能让您的项目焕发光彩',
+    PROXIO_FEATURE_TITLE: 'Pourquoi me choisir',
+    PROXIO_FEATURE_TEXT_1: 'Je peux faire briller votre projet',
     PROXIO_FEATURE_TEXT_2:
-        '丰富的案例经验，专业的技术服务，优质的沟通效率',
+        'Riche expérience de cas, service technique professionnel, efficacité de communication de qualité',
 
     // 特性1
     PROXIO_FEATURE_1_ICON_CLASS: 'fa-solid fa-stopwatch', // fas图标
     PROXIO_FEATURE_1_ICON_IMG_URL: '', // 图片图标选填，默认是fas图标，如果需要图片图标可以填写图片地址，示例/avatar.png
-    PROXIO_FEATURE_1_TITLE_1: '高效工作流程',
-    PROXIO_FEATURE_1_TEXT_1: '精简的设计流程确保快速交付，在紧迫的工期下仍能保证品质与细节不打折扣。',
+    PROXIO_FEATURE_1_TITLE_1: 'Flux de travail efficace',
+    PROXIO_FEATURE_1_TEXT_1: 'Un processus de conception simplifié garantit une livraison rapide, maintenant la qualité et les détails même sous des délais serrés.',
 
     PROXIO_FEATURE_2_ICON_CLASS: 'fa-solid fa-comments',
     PROXIO_FEATURE_2_ICON_IMG_URL: '', 
-    PROXIO_FEATURE_2_TITLE_1: '协作式流程',
-    PROXIO_FEATURE_2_TEXT_1: '与你紧密合作，融合反馈意见，打造超越预期的设计',
+    PROXIO_FEATURE_2_TITLE_1: 'Processus collaboratif',
+    PROXIO_FEATURE_2_TEXT_1: 'Collaboration étroite avec vous, intégrant les retours pour créer un design qui dépasse les attentes',
 
     PROXIO_FEATURE_3_ICON_CLASS: 'fa-solid fa-search',
     PROXIO_FEATURE_2_ICON_IMG_URL: '',
-    PROXIO_FEATURE_3_TITLE_1: '细节把控',
-    PROXIO_FEATURE_3_TEXT_1: '精益求精雕琢每个元素，确保成品精致统一，令人过目难忘',
+    PROXIO_FEATURE_3_TITLE_1: 'Maîtrise des détails',
+    PROXIO_FEATURE_3_TEXT_1: 'Perfectionnement minutieux de chaque élément, garantissant un résultat raffiné et unifié, mémorable à première vue',
 
-    PROXIO_FEATURE_BUTTON_TEXT: '了解更多', // 按钮文字
+    PROXIO_FEATURE_BUTTON_TEXT: 'En savoir plus', // 按钮文字
     PROXIO_FEATURE_BUTTON_URL: 'https://github.com/tangly1024/NotionNext', // 按钮跳转
 
     // 首页生涯区块
     PROXIO_CAREER_ENABLE: true, // 区块开关
-    PROXIO_CAREER_TITLE: '生涯',
+    PROXIO_CAREER_TITLE: 'Parcours',
     PROXIO_CAREER_TEXT:
-        '以下是我的职业生涯',
+        'Voici mon parcours professionnel',
 
     // 生涯内容卡牌 ，title是标题 ，bio是备注，text是详情
     PROXIO_CAREERS: [
-        { title: 'Freelance Architect', bio: '2016-2020', text: 'As a freelance architect, I worked on a range of residential and commercial projects, balancing form and function. I collaborated with clients and contractors to transform concepts into reality, ensuring each design was both aesthetically pleasing and structurally sound.' },
-        { title: 'Product Designer at Spotify', bio: '2020-2022', text: 'At Spotify, I helped shape innovative features for millions of users globally. My focus was on creating seamless music discovery experiences, enhancing user interfaces, and optimizing cross-platform navigation, which led to an improved product flow and increased user satisfaction.' },
-        { title: 'Freelance Product Designer', bio: '2022-Now', text: 'Now I design user-centric products that align with client visions. As a freelance product designer, I collaborate with startups and established companies, crafting solutions that elevate user experiences and increase engagement across both digital and physical interfaces.' }
+        { title: 'Architecte Freelance', bio: '2016-2020', text: 'En tant qu\'architecte freelance, j\'ai travaillé sur une gamme de projets résidentiels et commerciaux, équilibrant forme et fonction. J\'ai collaboré avec des clients et des entrepreneurs pour transformer les concepts en réalité, en m\'assurant que chaque design soit à la fois esthétiquement plaisant et structurellement solide.' },
+        { title: 'Designer Produit chez Spotify', bio: '2020-2022', text: 'Chez Spotify, j\'ai contribué à façonner des fonctionnalités innovantes pour des millions d\'utilisateurs dans le monde. Mon focus était de créer des expériences fluides de découverte musicale, d\'améliorer les interfaces utilisateur et d\'optimiser la navigation cross-plateforme, ce qui a conduit à une amélioration du flux produit et une satisfaction utilisateur accrue.' },
+        { title: 'Designer Produit Freelance', bio: '2022-Maintenant', text: 'Maintenant je conçois des produits centrés sur l\'utilisateur qui s\'alignent avec les visions des clients. En tant que designer produit freelance, je collabore avec des startups et des entreprises établies, créant des solutions qui élèvent les expériences utilisateur et augmentent l\'engagement à travers les interfaces numériques et physiques.' }
     ],
 
     // 首页用户测评区块
     PROXIO_TESTIMONIALS_ENABLE: true, // 测评区块开关
-    PROXIO_TESTIMONIALS_TITLE: '用户反馈',
-    PROXIO_TESTIMONIALS_TEXT_1: '我们的用户怎么说',
+    PROXIO_TESTIMONIALS_TITLE: 'Retours clients',
+    PROXIO_TESTIMONIALS_TEXT_1: 'Ce que disent nos utilisateurs',
     PROXIO_TESTIMONIALS_TEXT_2:
-        '数千位站长选择用NotionNext搭建他们的网站,通过帮助手册、交流社群以及技术咨询，大家成功上线了自己的网站',
+        'Des milliers de webmasters choisissent NotionNext pour construire leurs sites web. Grâce aux manuels d\'aide, aux communautés d\'échange et aux consultations techniques, tout le monde a réussi à mettre en ligne son site web',
 
     // 用户测评处的跳转按钮
     PROXIO_TESTIMONIALS_BUTTON_URL: '/about',
-    PROXIO_TESTIMONIALS_BUTTON_TEXT: '联系我',
+    PROXIO_TESTIMONIALS_BUTTON_TEXT: 'Me contacter',
 
     // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
     PROXIO_TESTIMONIALS_ITEMS: [
         {
             PROXIO_TESTIMONIALS_ITEM_TEXT:
-                '感谢大佬的方法。之前尝试过Super、Potion等国外的第三方平台，实现效果一般，个性化程度远不如这个方法，已经用起来了！ ',
+                'Merci à ce grand maître pour sa méthode. J\'ai essayé auparavant Super, Potion et d\'autres plateformes tierces étrangères, l\'effet était général, le degré de personnalisation est loin d\'égaler cette méthode, je l\'utilise déjà ! ',
             PROXIO_TESTIMONIALS_ITEM_AVATAR:
                 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F22de3fcb-d90d-4271-bc01-f815f476122b%2F4FE0A0C0-E487-4C74-BF8E-6F01A27461B8-14186-000008094BC289A6.jpg?table=collection&id=a320a2cc-6ebe-4a8d-95cc-ea94e63bced9&width=200',
             PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'Ryan_G',
-            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Ryan`Log 站长',
+            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Propriétaire de Ryan`Log',
             PROXIO_TESTIMONIALS_ITEM_URL: 'https://blog.gaoran.xyz/'
         },
         {
             PROXIO_TESTIMONIALS_ITEM_TEXT:
-                '很喜欢这个主题，本代码小白用三天台风假期搭建出来了，还根据大佬的教程弄了自定义域名，十分感谢，已请喝咖啡~',
+                'J\'adore ce thème, petit novice en code qui a réussi à le construire en trois jours de congés typhon, et j\'ai même configuré un nom de domaine personnalisé selon le tutoriel du maître, merci beaucoup, j\'ai déjà offert un café~',
             PROXIO_TESTIMONIALS_ITEM_AVATAR:
                 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0d33d169-f932-41ff-ac6b-88a923c08e02%2F%25E5%25A4%25B4%25E5%2583%258F.jfif?table=collection&id=7787658d-d5c0-4f34-8e32-60c523dfaba3&width=400',
             PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'Asenkits',
-            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '阿森的百宝袋 站长',
+            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Propriétaire de la Boîte à Trésors d\'Asen',
             PROXIO_TESTIMONIALS_ITEM_URL: 'https://asenkits.top/'
         },
         {
             PROXIO_TESTIMONIALS_ITEM_TEXT:
-                '呜呜呜，经过一个下午的努力，终于把博客部署好啦，非常感谢Tangly1024大佬的框架和教程，这是我有生之年用过的最好用的博客框架┭┮﹏┭┮。从今之后，我就可以在自己的博客里bb啦，( •̀ ω •́ )y ',
+                'Snif snif, après un après-midi d\'effort, j\'ai finalement réussi à déployer le blog, merci beaucoup au maître Tangly1024 pour le framework et le tutoriel, c\'est le meilleur framework de blog que j\'aie jamais utilisé de ma vie ┭┮﹏┭┮. À partir de maintenant, je peux papoter sur mon propre blog, ( •̀ ω •́ )y ',
             PROXIO_TESTIMONIALS_ITEM_AVATAR:
                 '/avatar.png',
             PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'DWIND',
-            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '且听风吟 站长',
+            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Propriétaire d\'Écouter le Vent Chanter',
             PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.dwind.top/'
         },
         {
             PROXIO_TESTIMONIALS_ITEM_TEXT:
-                '感谢提供这么好的项目哈哈 之前一直不知道怎么部署(别的项目好难好复杂)这个相对非常简单 新手非常友好哦',
+                'Merci de fournir un si bon projet haha, avant je ne savais pas comment déployer (les autres projets sont si difficiles et complexes), celui-ci est relativement très simple, très convivial pour les débutants',
             PROXIO_TESTIMONIALS_ITEM_AVATAR:
                 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd52f6766-3e32-4c3d-8529-46e1f214360f%2Ffavicon.svg?table=collection&id=7d76aad5-a2c4-4d9a-887c-c7913fae4eed&width=400',
-            PROXIO_TESTIMONIALS_ITEM_NICKNAME: '迪升disheng ',
-            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'AI资源分享 Blog',
+            PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'Disheng ',
+            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Blog de Partage de Ressources IA',
             PROXIO_TESTIMONIALS_ITEM_URL: 'https://blog.disheng.org/'
         },
         {
             PROXIO_TESTIMONIALS_ITEM_TEXT:
-                '灰常感谢大佬的博客项目，能将博客和notion结合起来，这一直是我挺期待的博客模式。',
+                'Merci énormément au maître pour le projet de blog, pouvoir combiner blog et notion, c\'est un mode de blog que j\'attendais depuis longtemps.',
             PROXIO_TESTIMONIALS_ITEM_AVATAR:
                 '/avatar.png',
             PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'AnJhon',
-            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Anjhon`s Blog 站长',
+            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Propriétaire d\'Anjhon`s Blog',
             PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.anjhon.top'
         },
         {
-            PROXIO_TESTIMONIALS_ITEM_TEXT: '用好久了，太感谢了',
+            PROXIO_TESTIMONIALS_ITEM_TEXT: 'Je l\'utilise depuis longtemps, merci beaucoup',
             PROXIO_TESTIMONIALS_ITEM_AVATAR:
                 '/avatar.png',
             PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'LUCEN',
-            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'LUCEN考验辅导 站长',
+            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Propriétaire de Conseil d\'Examen LUCEN',
             PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.lucenczz.top/'
         }
     ],
 
     //   FAQ 常见问题模块
     PROXIO_FAQ_ENABLE: true, // 常见问题模块开关
-    PROXIO_FAQ_TITLE: '常见问题解答',
-    PROXIO_FAQ_TEXT_1: '有任何问题吗？请看这里',
-    PROXIO_FAQ_TEXT_2: '我们收集了常见的用户疑问',
+    PROXIO_FAQ_TITLE: 'Questions Fréquemment Posées',
+    PROXIO_FAQ_TEXT_1: 'Vous avez des questions ? Regardez ici',
+    PROXIO_FAQ_TEXT_2: 'Nous avons collecté les questions courantes des utilisateurs',
     PROXIO_FAQS: [
-        { q: 'NotionNext有帮助文档吗？', a: 'NotionNext提供了<a href="https://docs.tangly1024.com/about" className="underline">帮助文档</a>，操作<a href="https://www.bilibili.com/video/BV1fM4y1L7Qi/" className="underline">演示视频</a>，以及<a href="https://docs.tangly1024.com/article/chat-community" className="underline">交流社群</a>来协助您完成网站的搭建部署' },
-        { q: '部署后要如何编写文章？', a: '您可以在Notion中之间添加或修改类型为Post的页面，内容将被实时同步在站点中，详情参考<a className="underline" href="https://docs.tangly1024.com/article/start-to-write">《帮助文档》</a>' },
-        { q: '站点部署失败，更新失败？', a: '通常是配置修改错误导致，请检查配置或者重试操作步骤，或者通过Vercel后台的Deployments中找到错误日志，并向网友求助' },
-        { q: '文章没有实时同步？', a: '先检查Notion_Page_ID是否正确配置，其次由于博客的每个页面都有独立缓存，刷新网页后即可解决' },
+        { q: 'NotionNext a-t-il une documentation d\'aide ?', a: 'NotionNext fournit une <a href="https://docs.tangly1024.com/about" className="underline">documentation d\'aide</a>, des <a href="https://www.bilibili.com/video/BV1fM4y1L7Qi/" className="underline">vidéos de démonstration</a>, ainsi qu\'une <a href="https://docs.tangly1024.com/article/chat-community" className="underline">communauté d\'échange</a> pour vous aider à compléter la construction et le déploiement du site web' },
+        { q: 'Comment écrire des articles après le déploiement ?', a: 'Vous pouvez ajouter ou modifier directement les pages de type Post dans Notion, le contenu sera synchronisé en temps réel sur le site, voir <a className="underline" href="https://docs.tangly1024.com/article/start-to-write">《Documentation d\'aide》</a> pour plus de détails' },
+        { q: 'Le déploiement du site a échoué, la mise à jour a échoué ?', a: 'C\'est généralement causé par une erreur de configuration, veuillez vérifier la configuration ou réessayer les étapes d\'opération, ou trouvez les journaux d\'erreur dans les Déploiements du backend Vercel et demandez de l\'aide aux internautes' },
+        { q: 'Les articles ne se synchronisent pas en temps réel ?', a: 'Vérifiez d\'abord si Notion_Page_ID est correctement configuré, ensuite comme chaque page du blog a un cache indépendant, actualiser la page web résoudra le problème' },
     ],
 
     // 关于作者区块
     PROXIO_ABOUT_ENABLE: true, // 关于作者区块区块开关
-    PROXIO_ABOUT_TITLE: '关于作者',
-    PROXIO_ABOUT_TEXT_1: 'I am an Architect Turned Into a Product Designer',
+    PROXIO_ABOUT_TITLE: 'À propos de l\'auteur',
+    PROXIO_ABOUT_TEXT_1: 'Je suis un Architecte Devenu Designer Produit',
     PROXIO_ABOUT_TEXT_2:
-        'With a background in architecture, I now apply my expertise to product design, blending aesthetics, functionality, and innovation. My goal is to create modern, user-focused designs that bring your vision to life.',
+        'Avec une formation en architecture, j\'applique maintenant mon expertise au design produit, alliant esthétique, fonctionnalité et innovation. Mon objectif est de créer des designs modernes et centrés sur l\'utilisateur qui donnent vie à votre vision.',
     PROXIO_ABOUT_PHOTO_URL: '/avatar.png',
-    PROXIO_ABOUT_KEY_1: '经验年限',
-    PROXIO_ABOUT_VAL_1: '10年+',
-    PROXIO_ABOUT_KEY_2: '客户',
+    PROXIO_ABOUT_KEY_1: 'Années d\'expérience',
+    PROXIO_ABOUT_VAL_1: '10+',
+    PROXIO_ABOUT_KEY_2: 'Clients',
     PROXIO_ABOUT_VAL_2: '1000+',
-    PROXIO_ABOUT_KEY_3: '交付项目',
+    PROXIO_ABOUT_KEY_3: 'Projets livrés',
     PROXIO_ABOUT_VAL_3: '5000+',
-    PROXIO_ABOUT_KEY_4: '累积创作时长（小时）',
+    PROXIO_ABOUT_KEY_4: 'Temps de création cumulé (heures)',
     PROXIO_ABOUT_VAL_4: '10000+',
 
     PROXIO_ABOUT_BUTTON_URL: '/about',
-    PROXIO_ABOUT_BUTTON_TEXT: '关于我',
+    PROXIO_ABOUT_BUTTON_TEXT: 'À propos de moi',
 
     // 横向滚动文字
     PROXIO_BRANDS_ENABLE: true, // 滚动文字
     PROXIO_BRANDS: [
-        'Web Design',
-        'Logo Design',
-        'Mobile App Design',
-        'Product Design'
+        'Design Web',
+        'Design de Logo',
+        'Design d\'Application Mobile',
+        'Design Produit'
     ],
 
-    PROXIO_FOOTER_SLOGAN: '我们通过技术为品牌和公司创造数字体验。',
+    PROXIO_FOOTER_SLOGAN: 'Nous créons des expériences numériques pour les marques et entreprises grâce à la technologie.',
 
     // 页脚三列菜单组
     // 页脚菜单
     PROXIO_FOOTER_LINKS: [
         {
-            name: '友情链接',
+            name: 'Liens Amis',
             menus: [
                 {
-                    title: 'Tangly的学习笔记',
+                    title: 'Notes d\'Apprentissage de Tangly',
                     href: 'https://blog.tangly1024.com'
                 },
                 {
@@ -203,54 +203,54 @@ const CONFIG = {
             ]
         },
         {
-            name: '开发者',
+            name: 'Développeur',
             menus: [
                 { title: 'Github', href: 'https://github.com/tangly1024/NotionNext' },
                 {
-                    title: '开发帮助',
+                    title: 'Aide au Développement',
                     href: 'https://docs.tangly1024.com/article/how-to-develop-with-notion-next'
                 },
                 {
-                    title: '功能反馈',
+                    title: 'Retours de Fonctionnalités',
                     href: 'https://github.com/tangly1024/NotionNext/issues/new/choose'
                 },
                 {
-                    title: '技术讨论',
+                    title: 'Discussions Techniques',
                     href: 'https://github.com/tangly1024/NotionNext/discussions'
                 },
                 {
-                    title: '关于作者',
+                    title: 'À propos de l\'Auteur',
                     href: 'https://blog.tangly1024.com/about'
                 }
             ]
         }],
 
-    PROXIO_FOOTER_BLOG_LATEST_TITLE: '最新文章',
+    PROXIO_FOOTER_BLOG_LATEST_TITLE: 'Articles Récents',
 
-    PROXIO_FOOTER_PRIVACY_POLICY_TEXT: '隐私政策',
+    PROXIO_FOOTER_PRIVACY_POLICY_TEXT: 'Politique de Confidentialité',
     PROXIO_FOOTER_PRIVACY_POLICY_URL: '/privacy-policy',
 
-    PROXIO_FOOTER_PRIVACY_LEGAL_NOTICE_TEXT: '法律声明',
+    PROXIO_FOOTER_PRIVACY_LEGAL_NOTICE_TEXT: 'Mentions Légales',
     PROXIO_FOOTER_PRIVACY_LEGAL_NOTICE_URL: '/legacy-notice',
 
-    PROXIO_FOOTER_PRIVACY_TERMS_OF_SERVICE_TEXT: '服务协议',
+    PROXIO_FOOTER_PRIVACY_TERMS_OF_SERVICE_TEXT: 'Conditions d\'Utilisation',
     PROXIO_FOOTER_PRIVACY_TERMS_OF_SERVICE_URL: '/terms-of-use',
 
     // 404页面的提示语
-    PROXIO_404_TITLE: '我们似乎找不到您要找的页面。',
-    PROXIO_404_TEXT: '抱歉！您要查找的页面不存在。可能已经移动或删除。',
-    PROXIO_404_BACK: '回到主页',
+    PROXIO_404_TITLE: 'Nous ne trouvons pas la page que vous recherchez.',
+    PROXIO_404_TEXT: 'Désolé ! La page que vous recherchez n\'existe pas. Elle a peut-être été déplacée ou supprimée.',
+    PROXIO_404_BACK: 'Retour à l\'accueil',
 
     // 页面底部的行动呼吁模块
     PROXIO_CTA_ENABLE: true,
-    PROXIO_CTA_TITLE: '与我建立联系',
-    PROXIO_CTA_TITLE_2: '让我们立刻启动您的项目',
+    PROXIO_CTA_TITLE: 'Établissons une connexion',
+    PROXIO_CTA_TITLE_2: 'Lançons immédiatement votre projet',
     PROXIO_CTA_DESCRIPTION:
-        '访问NotionNext的操作文档，我们提供了详细的教程，帮助你即刻搭建站点',
+        'Consultez la documentation opérationnelle de NotionNext, nous fournissons des tutoriels détaillés pour vous aider à construire votre site immédiatement',
     PROXIO_CTA_BUTTON: true, // 是否显示按钮
     PROXIO_CTA_BUTTON_URL:
         '/about',
-    PROXIO_CTA_BUTTON_TEXT: '联系我',
+    PROXIO_CTA_BUTTON_TEXT: 'Me contacter',
 
     PROXIO_POST_REDIRECT_ENABLE: true, // 默認開啟重定向
     PROXIO_POST_REDIRECT_URL: 'https://blog.tangly1024.com', // 重定向域名
